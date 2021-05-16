bae3b5b370d232d145cc97d48c687f386fc4fb49
ahmedKamal-eng <ahmedkamal17781@yahoo.com>
2021-05-16 06:02:35 +0300

    commit coin-ticker

---
+++ lib/coin_data.dart
@@ -0,0 +1,53 @@
+//2. Import the required packages.
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const List<String> currenciesList = [
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'INR',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'USD',
+  'ZAR'
+];
+
+const List<String> cryptoList = [
+  'BTC',
+  'ETH',
+  'LTC',
+];
+
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+const apiKey = 'ED4A9A14-F1A3-48F1-954E-0053E338431C';
+
+class CoinData {
+  //TODO 3: Update getCoinData to take the selectedCurrency as an input.
+  Future getCoinData(String crypto, String selectedCurrency) async {
+    //TODO 4: Update the URL to use the selectedCurrency input.
+    String requestURL = '$coinAPIURL/$crypto/$selectedCurrency?apikey=$apiKey';
+    http.Response response = await http.get(requestURL);
+    if (response.statusCode == 200) {
+      var decodedData = jsonDecode(response.body);
+      var lastPrice = decodedData['rate'];
+      return lastPrice;
+    } else {
+      print(response.statusCode);
+      throw 'Problem with the get request';
+    }
+  }
+}
